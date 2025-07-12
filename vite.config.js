22a2abc82b94852342b104340fed6fa91383ec97
Nosam <mtrdbd@gmail.com>
2025-07-12 02:03:26 -0400

    Update vite.config.js base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/website_testReact/', // Replace with your repo name
+  base: '/k-ville_test1/', // Replace with your repo name
 })
