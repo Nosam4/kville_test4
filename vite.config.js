9d4dc083d4c08813ffd357ccd1da813c8b3ffb95
Nosam <mtrdbd@gmail.com>
2025-07-12 02:09:34 -0400

    Update base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/k-ville_test1/', // Replace with your repo name
+  base: '/', // ✅ Serve from root for custom domain
 })
